de414cc9395a1a5b1594ad75b83b1c934369a4fc
sinnefa <sinnefa@gmail.com>
2022-02-19 16:28:06 +0100

    Added reverse parameter to sorting function

--- binaryspaghettisort.py
+++ binaryspaghettisort.py
@@ -12,7 +12,7 @@ class SparseList(list):
     try: return list.__getitem__(self, index)
     except IndexError: return None
 
-def BinarySpaghettiSort(values):
+def BinarySpaghettiSort(values, reverse=False):
   length = len(values)
   sorted = [0]*length
   maximum = max(values)
@@ -26,7 +26,15 @@ def BinarySpaghettiSort(values):
     binary_spaghetti_heads_index[(n-minimum)] += 1<<(length-1-i)
     
   index = 0
-  for xor in binary_spaghetti_heads_index:
+  start = 0
+  stop = len(binary_spaghetti_heads_index)
+  pace = 1
+  if reverse:
+    start = len(binary_spaghetti_heads_index)-1
+    stop = -1
+    pace = -1
+  for i in range(start,stop,pace):
+    xor = binary_spaghetti_heads_index[i]
     #if xor is None: # De-Comment to use sparse data structure
     #  continue # De-Comment to use sparse data structure
     while xor > 0:
@@ -45,3 +53,13 @@ if __name__ == "__main__":
 	sorted(values)
 	print("Python Sort 5000 random N+ numbers took: ",time.time() - start_time, "seconds")
 	print("Is Binary Spaghetti Sort equal to Python sort?: ", sorted(values)==vett)
+	print()
+	# reversed
+	values = [random.randint(1, 5000) for _ in range(5000)]
+	start_time = time.time()
+	vett = BinarySpaghettiSort(values,True)
+	print("Reversed Binary Spaghetti Sort 5000 random N+ numbers took: ",time.time() - start_time, "seconds")
+	start_time = time.time()
+	sorted(values,reverse=True)
+	print("Reversed Python Sort 5000 random N+ numbers took: ",time.time() - start_time, "seconds")
+	print("Is Binary Spaghetti Sort equal to Python sort?: ", sorted(values,reverse=True)==vett)
